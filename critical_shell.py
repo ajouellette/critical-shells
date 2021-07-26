06932591fc7df2c3de9b78001416348a02163121
Aaron Ouellette <aaronjo2@illinois.edu>
2021-07-26 16:21:28 -0500

    better critical shell algorithm

--- critical_shell.py
+++ critical_shell.py
@@ -3,6 +3,7 @@ from astropy.cosmology import FlatLambdaCDM
 import astropy.units as u
 import pickle
 import h5py
+import time
 import snapshot
 
 def v_radial(pos, vel, radii=None):
@@ -11,6 +12,7 @@ def v_radial(pos, vel, radii=None):
         radii = np.linalg.norm(pos, axis=1)
     unit_vectors = pos / np.expand_dims(radii, 1)
     return np.multiply(vel, unit_vectors).sum(1)
+
 
 def vr_physical(cosmo, pos, vel, scale_factor):
     """Calculate physical radial velocities wrt origin.
@@ -22,89 +24,97 @@ def vr_physical(cosmo, pos, vel, scale_factor):
     hubble_flow = cosmo.H(scale_factor) * scale_factor * radii
     return vr_peculiar + hubble_flow
 
+
 def get_density(radii, cut_radius, part_mass, a=1):
     num_particles = np.sum(radii < cut_radius)
     volume = 4/3 * np.pi * (cut_radius * a)**3
     return num_particles * part_mass / volume
 
+
 def get_mass(radius, density, a=1):
     volume = 4/3 * np.pi * (radius * a)**3
     return density * volume
 
-def critical_shell(pos, center, part_mass, crit_dens, crit_ratio=2, center_tol=1e-3, rcrit_tol=5e-4):
+
+def critical_shell(pos, center, part_mass, crit_dens, crit_ratio=2, center_tol=1e-3, rcrit_tol=5e-4, maxiters=100):
     """Find a critical shell starting from given center."""
     radius = 0.1
-    print(center)
-    # find center of largest structure
+    # find center of largest substructure
     center_converged = False
     for i in range(10):
         radii = np.linalg.norm(pos - center, axis=1)
         new_center = np.mean(pos[radii < radius], axis=0)
-        print(new_center, np.linalg.norm(new_center - center))
+        #print(new_center, np.linalg.norm(new_center - center))
         if np.linalg.norm(center - new_center) < center_tol:
             center_converged = True
             break
         center = new_center
         radius += 0.05
 
-    # decrease radius to get critical shell
-    radius = 4e-2
+    # decrease radius and then grow to get critical shell
+    radius = 1e-3
     radii = np.linalg.norm(pos - center, axis=1)
+    dr = rcrit_tol * 100
+    iters = 0
     density_converged = False
-    # decide whether to grow or shrink sphere
-    if get_density(radii, radius, part_mass, a=100) > crit_ratio*crit_dens:
-        dr = rcrit_tol
-    else:
-        dr = -rcrit_tol
-    for i in range(100):
-        center += np.mean(pos[radii < radius] - center, axis=0)
-        #print(center, radius)
-        radii = np.linalg.norm(pos - center, axis=1)
-        density = get_density(radii, radius, part_mass, a=100)
-        #print("{:.3f}".format(density / crit_dens))
-        if density <= crit_ratio * crit_dens and density > crit_dens:
-            density_converged = True
+    while dr > rcrit_tol:
+        iters += 1
+        new_center = center + np.mean(pos[radii < radius + dr] - center, axis=0)
+        new_radii = np.linalg.norm(pos - new_center, axis=1)
+        density = get_density(new_radii, radius+dr, part_mass, a=100)
+
+        if density < crit_ratio * crit_dens:
+            dr /= 2
+        else:
+            center = new_center
+            radii = new_radii
+            radius += dr
+        if iters > maxiters:
             break
-        radius += dr
+    
+    # convergence check (maybe not necessary)
+    if density < crit_ratio * crit_dens and density > crit_dens and dr < rcrit_tol:
+        density_converged = True
 
-    return center, radius, center_converged*density_converged
+    return center, radius, center_converged, density_converged
 
-
-def crit_shell_from_subhalos(fof_tab, min_mass=1e12):
-    fof_tab_data = h5py.File(fof_tab)
-    sh_masses = fof_tab_data["Subhalo"]["SubhaloMass"]
-    sh_pos = fof_tab_data["Subhalo"]["SubhaloCM"]
-
-    for i in range(len(sh_masses)):
-        if sh_masses * 1e10 < min_mass:
-            continue
-
-        # ignore centers too close to box boundary
-        if np.sum(center < 2) or np.sum(center > pd.box_size - 2):
-            print()
-            print("Skipping ", center)
-            print()
-            continue
-
-        critical_shell(pos, sh_pos[i], )
-
-    return
 
 
 if __name__ == "__main__":
-    pd = snapshot.ParticleData("/home/aaron/dm-l50-n128-a100/snapshot_012.hdf5")
-    hc = snapshot.HaloCatalog("/home/aaron/dm-l50-n128-a100/fof_tab_012.hdf5")
+    pd = snapshot.ParticleData("/projects/caps/aaronjo2/dm-l256-n256-a100/snapshot_021.hdf5")
+    #hc = snapshot.HaloCatalog("/home/aaron/dm-l50-n128-a100/fof_tab_012.hdf5")
+    fof_tab_data = h5py.File("/projects/caps/aaronjo2/dm-l256-n256-a100/fof_subhalo_tab_021.hdf5", 'r')
+    sh_masses = fof_tab_data["Subhalo"]["SubhaloMass"][:] * 1e10
+    sh_pos = fof_tab_data["Subhalo"]["SubhaloCM"][:]
+    fof_tab_data.close()
+
+    print("Particle mass: {:.3e}".format(pd.part_mass))
 
     # Use H0 = 100 to factor out h
     cosmo = FlatLambdaCDM(H0=100, Om0=pd.OmegaMatter)
     crit_dens_a100 = cosmo.critical_density(-0.99).to(u.Msun / u.Mpc**3).value
+    print("Critical density at a = 100: {:.3e}".format(crit_dens_a100))
 
-    # use x most massive halos
-    centers = hc.pos[:300]
     radii = []
     centers_s = []
 
-    for center in centers:
+    min_mass = 1e14
+    print("{:} subhalos above min mass".format(np.sum(sh_masses > min_mass)))
+
+    halos_found = 0
+    sh_processed = 0
+    time_start = time.perf_counter()
+    for i in range(len(sh_masses)):
+        if sh_masses[i] < min_mass:
+            continue
+
+        if sh_processed % 10 == 0 and sh_processed != 0:
+            time_end = time.perf_counter()
+            print(sh_processed, halos_found, time_end - time_start)
+            time_start = time_end
+
+        center = sh_pos[i]
+
         # ignore centers too close to box boundary
         if np.sum(center < 2) or np.sum(center > pd.box_size - 2):
             print()
@@ -112,16 +122,22 @@ if __name__ == "__main__":
             print()
             continue
 
-        center_s, radius, converged = critical_shell(pd.pos, center, pd.part_mass, crit_dens_a100)
+        center_s, radius, c_converged, d_converged = critical_shell(pd.pos, center, pd.part_mass, crit_dens_a100)
         if np.sum(center_s < 2) or np.sum(center_s > pd.box_size - 2):
             print()
             print("Skipping ", center)
             print()
             continue
 
-        if converged:
+        if c_converged * d_converged:
+            print("Found halo:", center, radius)
             radii.append(radius)
             centers_s.append(center_s)
+            halos_found += 1
+        else:
+            print("center converged", c_converged, "density converged", d_converged, radius)
+
+        sh_processed += 1
 
         #print(critical_shell(pd.pos, center, pd.part_mass, crit_dens_a100))
 
