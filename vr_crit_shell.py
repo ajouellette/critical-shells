6fd040cca3f1aa688abf24baa087c8b88fc68f8b
Aaron Ouellette <aaronjo2@illinois.edu>
2021-11-08 16:06:15 -0600

    use ball tree and refactor

--- vr_crit_shell.py
+++ vr_crit_shell.py
@@ -1,16 +1,19 @@
+import os
+import sys
+import pickle
 import numpy as np
+import numba as nb
 from mpi4py import MPI
 from astropy.cosmology import FlatLambdaCDM
 import astropy.units as u
-import pickle
+from sklearn import neighbors
 import h5py
-import time
-import sys
-import os
-from numba import jit
 import snapshot
+from utils import mean_pos
 
-@jit(nopython=True)
+import time
+
+@nb.njit
 def find_first(value, vector):
     """Find the index of the first occurence of value in vector."""
     for i, v in enumerate(vector):
@@ -18,7 +21,7 @@ def find_first(value, vector):
             return i
     return -1
 
-@jit(nopython=True)
+@nb.njit
 def find_first_npositive(vector):
     """Find index of first non-positive value in vector."""
     prod = 1
@@ -27,6 +30,19 @@ def find_first_npositive(vector):
         if prod <= 0:
             break
     return i
+
+
+@nb.njit
+def calc_vr_phys(pos, vel, center, radius, a, H, h):
+    pos_centered = pos - center
+    p_radii = np.sqrt(np.sum(pos_centered**2, axis=1))
+    vel_center = mean_pos(vel[p_radii < radius])
+    vel_peculiar = np.sqrt(a) * (vel - vel_center)
+    unit_vectors = pos_centered / np.expand_dims(p_radii, 1)
+    vr_peculiar = np.multiply(vel_peculiar, unit_vectors).sum(1)
+    hubble_flow = H * a * p_radii / h
+    vr_physical = vr_peculiar + hubble_flow
+    return p_radii, vr_physical
 
 
 if __name__ == "__main__":
@@ -48,13 +64,19 @@ if __name__ == "__main__":
             print("Error: could not find data file")
         sys.exit(1)
     # read particle data on all processes
-    particles = snapshot.ParticleData(snap_file)
+    pd = snapshot.ParticleData(snap_file)
+    pos_tree = neighbors.BallTree(pd.pos)
 
-    cosmo = FlatLambdaCDM(H0=particles.Hubble0, Om0=particles.OmegaMatter)
+    comm.Barrier()
+    if rank == 0:
+        print("Memory usage after loading particle data and constructing trees")
+        os.system("free -h")
+
+    cosmo = FlatLambdaCDM(H0=pd.Hubble0, Om0=pd.OmegaMatter)
 
     if rank == 0:
-        print("h = {:.3f}, a = {:.1f}".format(cosmo.h, particles.a))
-        print("H = {:.2f}  {:.2f}".format(particles.Hubble, cosmo.H(-0.99)))
+        print("h = {:.3f}, a = {:.1f}".format(cosmo.h, pd.a))
+        print("H = {:.2f}  {:.2f}".format(pd.Hubble, cosmo.H(-0.99)))
 
     # read all halo data on rank 0 and divide up work
     if rank == 0:
@@ -85,31 +107,20 @@ if __name__ == "__main__":
         time_start = time.perf_counter()
 
         center = centers[i]
-        radius = radii[i] / cosmo.h 
-        r_cut = 1.5 * radius
-        # calculate physical radial velocities of particles inside r_cut
-        p_radii = np.linalg.norm(particles.pos - center, axis=1) / cosmo.h
-        mask = p_radii < r_cut
-        vel_center = np.mean(particles.vel[p_radii < radius], axis=0)
-        pos_cut = (particles.pos[mask] - center) / cosmo.h
-        vel_cut = np.sqrt(particles.a) * (particles.vel[mask] - vel_center)
-        p_radii_cut = p_radii[mask]
-        unit_vectors = pos_cut / np.expand_dims(p_radii_cut, 1)
-        vr_peculiar = np.multiply(vel_cut, unit_vectors).sum(1)
-        hubble_flow = particles.Hubble * particles.a * p_radii_cut
-        vr_physical = vr_peculiar + hubble_flow
-       
-        time_mid = time.perf_counter()
+        radius = radii[i]
+        r_cut = 2 * radius
+        mask = pos_tree.query_radius(center.reshape(1,-1), r_cut)[0]
+        p_radii_cut, vr_physical = calc_vr_phys(pd.pos[mask], pd.vel[mask], center, radius, pd.a, pd.Hubble, cosmo.h)
 
         # find first radius after which vr_physical is no longer negative
         sorted_i = np.lexsort((vr_physical, p_radii_cut))
         index = find_first(0, np.cumprod(vr_physical[sorted_i][::-1] > 0))
         # average of last negative and next
-        radii_vr[i] = 0.5 * (p_radii_cut[sorted_i][::-1][index] + p_radii_cut[sorted_i][::-1][index-1]) * cosmo.h
+        radii_vr[i] = 0.5 * (p_radii_cut[sorted_i][::-1][index] + p_radii_cut[sorted_i][::-1][index-1])
 
         time_end = time.perf_counter()
 
-        print(i, radii_vr[i], " {:.4f} {:.4f}".format(time_mid - time_start, time_end - time_mid))
+        print(i, radii_vr[i], "  {:.4f} sec".format(time_end - time_start))
 
     comm.Barrier()
     if rank == 0:
@@ -117,9 +128,9 @@ if __name__ == "__main__":
         all_radii_vr = np.zeros_like(all_radii)
     else:
         all_radii_vr = None
-        
+
     comm.Gatherv(radii_vr, [all_radii_vr, count, displ, MPI.DOUBLE], root=0)
-        
+
     if rank == 0:
         print("Gathered")
         with open(data_dir + "-analysis/vr_crit_radii", 'wb') as f:
