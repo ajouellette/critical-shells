2c534c0fb572ad314d55632619890326eae7b78f
Aaron Ouellette <aaronjo2@illinois.edu>
2021-12-28 11:40:28 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -10,17 +10,19 @@ if __name__ == "__main__":
         version=VERSION,
         author="Aaron Ouellette",
         author_email="aaronjo2@illinois.edu",
-        description="",
-        long_description="",
+        license="LICENSE",
+        description="GADGET simulation analysis",
+        long_description=open("README.md").read(),
         install_requires=["numpy>=1.20",
                           "scipy>=1.7",
-                          "sklearn>=1.0",
+                          "scikit-learn>=1.0",
                           "numba>=0.54",
                           "h5py>=3.4",
                           "mpi4py>=3.1",
                           "astropy>=4.3",
-                          "pyfof>=0.2"
+                          "pyfof==0.2.dev0"
                           ],
         packages=["gadgetutils"],
+        scripts=["scripts"],
         url="https://github.com/ajouellette/critical-shells",
     )
