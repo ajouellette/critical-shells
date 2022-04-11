7efcf9975482432e56b7fb4b2e4f700bd152b55a
Aaron Ouellette <aaronjo2@illinois.edu>
2022-04-11 17:39:09 -0500

    save more energy info

--- scripts/energy.py
+++ scripts/energy.py
@@ -27,8 +27,12 @@ def main():
         print(f"Using data directory {data_dir}, found {len(files)} output files.")
 
         a_vals = []
+        all_all_t_vals = []
+        all_all_u_vals = []
+        all_all_n_parts = []
         all_all_tu_vals = []
         avg_tu = []
+        med_tu = []
         std_tu = []
 
     for f in files:
@@ -64,26 +68,36 @@ def main():
 
         comm.Bcast(shuffle, root=0)
 
+        t_vals = np.zeros(count[rank])
+        u_vals = np.zeros(count[rank])
         tu_vals = np.zeros(count[rank])
+        n_parts = np.zeros(count[rank], dtype=int)
+
         time_start1 = time.perf_counter()
         for i in range(count[rank]):
             halo_ind = shuffle[displ[rank] + i]
             inds = hc.get_particles(halo_ind, subhalo=subhalos)
+
             pos = pd.pos[inds]
             pos_center = mean_pos_pbc(pos, pd.box_size)
-            pos = pd.a * center_box_pbc(pos, pos_center, pd.box_size)
+            pos = center_box_pbc(pos, pos_center, pd.box_size)
+
             vel = pd.vel[inds]
-            vel_center = np.mean(vel, axis=0)
-            vel = np.sqrt(pd.a) * (vel - vel_center)
+            vel_center = np.mean(vel, axis=0, dtype=float)
+            vel = np.sqrt(pd.a) * (vel - vel_center)# + pd.Hubble * pos / pd.h
 
             time_start2 = time.perf_counter()
             T = energy.calc_kinetic(vel)
-            U = energy.calc_potential(pos, pd.part_mass / 1e10, G_sim_units)
+            epsilon = 1.2e-2 if pd.a < 1 else 1.2e-2 / pd.a
+            U = 1/pd.a * energy.calc_potential(pos, pd.part_mass / 1e10, G_sim_units, epsilon=epsilon)
             time_end2 = time.perf_counter()
             time_ms = (time_end2 - time_start2) * 1e3
 
             if halo_ind < 10:
                 print(f"rank {rank} halo {halo_ind} {len(inds)} particles {T/U:.3f} in {time_ms:.2f} ms")
+            t_vals[i] = T
+            u_vals[i] = U
+            n_parts[i] = len(inds)
             tu_vals[i] = T/U
 
         time_end1 = time.perf_counter()
@@ -93,21 +107,35 @@ def main():
         comm.Barrier()
 
         if rank == 0:
+            all_t_vals = np.zeros(Nhalos)
+            all_u_vals = np.zeros(Nhalos)
+            all_n_parts = np.zeros(Nhalos, dtype=int)
             all_tu_vals = np.zeros(Nhalos)
         else:
+            all_t_vals = None
+            all_u_vals = None
+            all_n_parts = None
             all_tu_vals = None
 
+        comm.Gatherv(t_vals, [all_t_vals, count, displ, MPI.DOUBLE], root=0)
+        comm.Gatherv(u_vals, [all_u_vals, count, displ, MPI.DOUBLE], root=0)
         comm.Gatherv(tu_vals, [all_tu_vals, count, displ, MPI.DOUBLE], root=0)
+        comm.Gatherv(n_parts, [all_n_parts, count, displ, MPI.LONG], root=0)
 
         if rank == 0:
             print("Gathered")
             print(np.min(all_tu_vals), np.max(all_tu_vals))
-            print(f"Average {np.mean(all_tu_vals)}")
+            print(f"Average {np.mean(all_tu_vals)}, Median {np.median(all_tu_vals)}")
             print(f"Std dev {np.std(all_tu_vals)}")
 
             a_vals.append(pd.a)
-            all_all_tu_vals.append(all_tu_vals[np.argsort(shuffle)])
+            unshuffle = np.argsort(shuffle)
+            all_all_t_vals.append(all_t_vals[unshuffle])
+            all_all_u_vals.append(all_u_vals[unshuffle])
+            all_all_n_parts.append(all_n_parts[unshuffle])
+            all_all_tu_vals.append(all_tu_vals[unshuffle])
             avg_tu.append(np.mean(all_tu_vals))
+            med_tu.append(np.median(all_tu_vals))
             std_tu.append(np.std(all_tu_vals))
 
     if rank == 0:
@@ -116,8 +144,12 @@ def main():
         with h5py.File(out_file, 'w') as f:
             f.create_dataset("ScaleFactors", data=a_vals)
             f.create_dataset("AvgRatio", data=avg_tu)
+            f.create_dataset("MedRatio", data=med_tu)
             f.create_dataset("StdRatio", data=std_tu)
             for i in range(len(a_vals)):
+                f.create_dataset(f"Kinetic_a{i}", data=all_all_t_vals[i])
+                f.create_dataset(f"Potential_a{i}", data=all_all_u_vals[i])
+                f.create_dataset(f"Nparticles_a{i}", data=all_all_n_parts[i])
                 f.create_dataset(f"Ratios_a{i}", data=all_all_tu_vals[i])
         print("Done.")
 
